c673505f7ae883244e1bda07f5de79c3d08d3ee3
raulsposito <raulsposito@gmail.com>
2021-09-28 21:32:25 -0300

    add navbar and style fade in on scroll

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import './App.css';
 import Row from './Row'
 import Banner from './Banner'
+import Nav from './Nav'
 import requests from './requests'
 
 function App() {
   return (
     <div className="App">
-      <h2>I'm blank</h2>
+      <Nav />
+      <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Lastest Movies" fetchUrl={requests.fetchLatest}/>
+      <Row title="Now Playing" fetchUrl={requests.fetchNowPlaying}/>
     </div>
   );
 }
 
 export default App;
-
-
-// import './App.css';
-// import Row from './Row'
-// import Banner from './Banner'
-// import requests from './requests'
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Banner />
-//       <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
-//       <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-//       <Row title="Lastest Movies" fetchUrl={requests.fetchLatest}/>
-//       <Row title="Now Playing" fetchUrl={requests.fetchNowPlaying}/>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/Banner.js
+++ src/Banner.js
@@ -34,15 +34,17 @@ function Banner() {
             }}
         >
             <div className="banner_contents">
-                <h1>{movie?.title || movie?.name || movie?.origina_name}</h1>
+                <h1 className="banner_title">{movie?.title || movie?.name || movie?.origina_name}</h1>
+
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+
+                <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
             </div>
 
-            <div className="banner_buttons">
-                <button className="banner_button">Play</button>
-                <button className="banner_button">My List</button>
-            </div>
-
-            <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+            <div className="banner_fadeBottom" />
         </header>
     )
 }
